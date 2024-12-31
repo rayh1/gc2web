f35abccf30a28f7f516d6ff6389352a3624dfa64
raymond <raymond-prive@charm.nl>
2024-12-31 10:32:07 +0000

    Pass GedcomTransmission reference to GedcomIterator.

--- GedcomTransmission.py
+++ GedcomTransmission.py
@@ -3,27 +3,27 @@ from typing import List
 from GedcomLine import GedcomLine
 
 class GedcomIterator:
-    def __init__(self, id_map: dict[str, GedcomLine], lines: List[GedcomLine], tag:str | None, value_re: str | None, follow_pointers: bool = True):
-        self.id_map: dict[str, GedcomLine] = id_map
+    def __init__(self, transmission: 'GedcomTransmission', lines: List[GedcomLine], tag:str | None, value_re: str | None, follow_pointers: bool = True):
+        self.transmission: 'GedcomTransmission' = transmission
         self.lines: List[GedcomLine] = lines
         self.tag: str | None = tag
         self.value_re: str | None = value_re
         self.follow_pointers: bool = follow_pointers
-        self.index: int = 0
+
+        self.cur_index: int = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        for i, line in enumerate(self.lines[self.index:], start=self.index):
-            self.index = i + 1
+        for i, line in enumerate(self.lines[self.cur_index:], start=self.cur_index):
+            self.cur_index = i + 1
 
             if self.tag and self.tag != line.tag:
                 continue
 
             if self.follow_pointers:
-                while line and line.pointer_value:
-                    line = self.id_map[line.pointer_value]
+                line = self.transmission.follow_pointers(line)
 
             if self.value_re != None:
                 if line.value == None:
@@ -43,9 +43,15 @@ class GedcomTransmission:
 
     def iterate(self, line: GedcomLine = None, tag: str | None = None, value_re: str | None = None, follow_pointers: bool | None = None) -> GedcomIterator:
         if line == None:
-            return GedcomIterator(self.id_map, self.all_lines, tag, value_re, follow_pointers)
+            return GedcomIterator(self, self.all_lines, tag, value_re, follow_pointers)
         else:
-            return GedcomIterator(self.id_map, line.sublines, tag, value_re, follow_pointers)
+            return GedcomIterator(self, line.sublines, tag, value_re, follow_pointers)
 
     def iterate_id(self, id: str, tag: str | None = None, value_re: str | None = None, follow_pointers: bool | None = None) -> GedcomIterator:
-            return GedcomIterator(self.id_map, self.id_map[id].sublines, tag, value_re, follow_pointers)
+            return GedcomIterator(self, self.id_map[id].sublines, tag, value_re, follow_pointers)
+    
+    def follow_pointers(self, line: GedcomLine) -> GedcomLine:
+        while line and line.pointer_value:
+            line = self.id_map[line.pointer_value]
+
+        return line
